da2f98d5fdedbc8a024f7b5f6c516991f5c33ac2
holaqz <arturhajtbaev2260@gmail.com>
2025-11-24 15:19:11 +0500

    calendar

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,122 +1,210 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(Calendar());
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _CalendarState extends State<Calendar> {
 
-  // This widget is the root of your application.
+  List<DateTime?> _getCalendarDays() {
+    final List<DateTime?> days = [];
+    
+    final firstDayOfMonth = DateTime(_currentDate.year, _currentDate.month, 1);
+    final lastDayOfMonth = DateTime(_currentDate.year, _currentDate.month + 1, 0);
+    final firstWeekday = firstDayOfMonth.weekday;
+
+    for (int i = 1; i < firstWeekday; i++) {
+      days.add(null);
+    }
+    
+    for (int day = 1; day <= lastDayOfMonth.day; day++) {
+      days.add(DateTime(_currentDate.year, _currentDate.month, day));
+    }
+    
+    final totalCells = 42;
+    while (days.length < totalCells) {
+      days.add(null);
+    }
+    
+    return days;
+  }
+
+  DateTime _currentDate = DateTime.now();
+  final DateTime _today = DateTime.now();
+  
+  String get _displayedMonth {
+    const months = [
+      'Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'
+    ];
+    return '${months[_currentDate.month - 1]} ${_currentDate.year}';
+  }
+
+    bool _isToday(DateTime date) {
+    final now = DateTime.now();
+    return date.year == now.year && date.month == now.month && date.day == now.day;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      theme: ThemeData(primaryColor: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 175, 230, 255),
+          title: Text('Календарь'),
+          ),
+        body: Container(
+          color: const Color.fromARGB(255, 175, 230, 255),
+          margin: EdgeInsets.only(top: 20, bottom: 300, right: 20, left: 20),
+          padding: EdgeInsetsGeometry.only(top:30, left: 30, right: 30, bottom: 30),
+          child:
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+              Text('${_currentDate.year}'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  IconButton(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    onPressed: () {
+                      _goToPreviousMonth();
+                    },
+                    icon: Icon(Icons.arrow_back_ios),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(vertical: 12, horizontal: 30),
+                    child:
+                      Text(
+                        _displayedMonth, 
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20),
+                        ),
+                    ),
+                  IconButton(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    onPressed: () {
+                      _goToNextMonth();
+                    },
+                    icon: Icon(Icons.arrow_forward_ios),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Пн', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Вт', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Ср', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Чт', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Пт', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Сб', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                  Padding(
+                    padding: EdgeInsetsGeometry.symmetric(horizontal: 12, vertical: 10),
+                    child:
+                    Text('Вс', style: TextStyle(fontWeight: FontWeight.w300)),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 7,
+                    childAspectRatio: 1.0,
+                  ),
+                  itemCount: _getCalendarDays().length,
+                  itemBuilder: (context, index) {
+                    final day = _getCalendarDays()[index];
+                    return Container(
+                      margin: const EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        color: day == null 
+                            ? Colors.transparent 
+                            : _isToday(day) 
+                                ? Colors.blue 
+                                : Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(
+                          color: day != null && _isToday(day) 
+                              ? Colors.blue 
+                              : Color.fromARGB(255, 175, 230, 255),
+                        ),
+                      ),
+                      child: day == null 
+                        ? null 
+                        : Center(
+                            child: Text(
+                              '${day.day}',
+                              style: TextStyle(
+                                color: _isToday(day) 
+                                    ? Colors.white
+                                    : Colors.black,
+                                fontWeight: _isToday(day)
+                                    ? FontWeight.bold
+                                    : FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+              OutlinedButton(
+                onPressed: _goToCurrentMonth, 
+                child: Text('Вернуться к сегодняшнему дню'),
+              )
+            ],
+            )
+          ), 
+        ),
+      );
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+    
+  void _goToPreviousMonth() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentDate = DateTime(_currentDate.year, _currentDate.month - 1);
     });
   }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void _goToNextMonth() {
+    setState(() {
+      _currentDate = DateTime(_currentDate.year, _currentDate.month + 1);
+    });
+  }
+  void _goToCurrentMonth() {
+    setState(() {
+      _currentDate = DateTime(_today.year, _today.month);
+    });
   }
 }
